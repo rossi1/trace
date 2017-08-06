ecb5670f955ad1329876d29d0c8619b01c469d25
rossi1 <emmanuelukwuegbu2016@gmail.com>
2017-08-06 04:41:00 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,17 @@ from config import DevelopmentConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
-from flask_moment import Moment
+from flask_mail import Mail
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
 db = SQLAlchemy(app)
-moment = Moment(app)
 bootstrap = Bootstrap(app)
 login_manager = LoginManager(app)
+mail = Mail(app)
+csrf = CSRFProtect(app)
 login_manager.login_view = 'login'
 login_manager.session_protection = 'strong'  # session protection to help prevent your users’ sessions from being stolen.
 
