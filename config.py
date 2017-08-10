f0092ad3d042b58c4db3ce5c01c4cbaf652affb0
rossi1 <emmanuelukwuegbu2016@gmail.com>
2017-08-10 02:35:15 +0100

    Update config.py

--- config.py
+++ config.py
@@ -23,15 +23,15 @@ class DevelopmentConfig:
     # RecaptchaField config
 
     RECAPTCHA_PUBLIC_KEY = '6LfFNysUAAAAAH8XvHjiSSpCpxrJc95vI-uN5Swy'
-    RECAPTCHA_PRIVATE_KEY = '6LfFNysUAAAAAHbiD_yVYEG9rZfrft48x9VPHzl6'  # this key should be kept secret
+    RECAPTCHA_PRIVATE_KEY = os.environ.get('key')  # this key should be kept secret
 
     # Mail Settings
 
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_USE_TLS = True
     MAIL_PORT = 587
-    MAIL_USERNAME = 'emmanuelukwuegbu2016@gmail.com'
-    MAIL_PASSWORD = '@1OBINNN'
+    MAIL_USERNAME = os.environ.get('username')
+    MAIL_PASSWORD = os.environ.get('password')
     MAIL_DEFAULT_SENDER = 'myapp@app.com'
 
     SECURITY_PASSWORD_SALT = 'password-reset-salt' # salt used for forgot password
