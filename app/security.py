ff9d683ac4ada8dbf0f0bd1219aaf01aa70fac33
rossi1 <emmanuelukwuegbu2016@gmail.com>
2017-08-10 02:11:55 +0100

    Update security.py

--- app/security.py
+++ app/security.py
@@ -4,17 +4,19 @@ from app import app
 
 def generate_confirmation_token(email):
     ts = Serializer(app.config['SECRET_KEY'])
-    return ts.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+    return ts.dumps(email, salt=app.config['SECURITY_EMAIL_SALT'])
 
 
 def confirm_token(token, expiration=84600):
     ts = Serializer(app.config['SECRET_KEY'])
-
-    try:
-        email = ts.loads(token, salt=app.config['SECURITY_PASSWORD_SALT'], max_age=expiration)
-
-    except:
-        return False
-    return email
+    return ts.loads(token, salt=app.config['SECURITY_EMAIL_SALT'], max_age=expiration)
 
 
+def generate_recovery_token(email):
+    ts = Serializer(app.config['SECRET_KEY'])
+    return ts.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+
+
+def confirm_recovery_token(token, expiration=3600):
+    ts = Serializer(app.config['SECRET_KEY'])
+    return ts.loads(token, salt=app.config['SECURITY_PASSWORD_SALT'], max_age=expiration)
