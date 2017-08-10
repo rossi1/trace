b6c4e26858457f37fbdeb5348a88a1219500e91d
rossi1 <emmanuelukwuegbu2016@gmail.com>
2017-08-10 02:14:09 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-basdir = os.path.abspath(os.path.dirname(__file__))
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class DevelopmentConfig:
@@ -11,25 +12,27 @@ class DevelopmentConfig:
     DEBUG = True
     # flask_wtf configs
     WTF_CSRF_ENABLED = True  # this protects forms against cross-site forgery
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'  # generate a standard secret key and also Used to securely sign the token
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    # generate a standard secret key and als
+    #  Used to securely sign the token
 
     # sqlalchemy config using sqlite
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basdir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # RecaptchaField config
 
     RECAPTCHA_PUBLIC_KEY = '6LfFNysUAAAAAH8XvHjiSSpCpxrJc95vI-uN5Swy'
-    RECAPTCHA_PRIVATE_KEY = os.environ.get('private key')  # this key should be kept secret
+    RECAPTCHA_PRIVATE_KEY = '6LfFNysUAAAAAHbiD_yVYEG9rZfrft48x9VPHzl6'  # this key should be kept secret
 
     # Mail Settings
 
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_USE_TLS = True
     MAIL_PORT = 587
-    MAIL_USERNAME = os.environ.get('username')
-    MAIL_PASSWORD = os.environ.get('password')
+    MAIL_USERNAME = 'emmanuelukwuegbu2016@gmail.com'
+    MAIL_PASSWORD = '@1OBINNN'
     MAIL_DEFAULT_SENDER = 'myapp@app.com'
 
-    SECURITY_PASSWORD_SALT = 'my_precious_two'  # used for the token
-
+    SECURITY_PASSWORD_SALT = 'password-reset-salt' # salt used for forgot password
+    SECURITY_EMAIL_SALT = 'email-confirmation-salt'  # salt used for email confirmation
