c6443ed1a5735a7b6c9dd493a862b0c68e013143
rossi1 <emmanuelukwuegbu2016@gmail.com>
2017-08-16 01:48:21 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash, abort, g, jsonify, make_response
-from .forms import RegistrationForm, LoginForm, EmailForm, PasswordForm, CompleteForm
+from .forms import RegistrationForm, LoginForm, EmailForm, PasswordForm, CompleteForm, ResendEmailForm
 from .models import User, PrivateDetails
 from app import db
 from flask_login import login_required, current_user, login_user,  logout_user
-from .security import generate_confirmation_token, confirm_token
+from .security import generate_confirmation_token, confirm_token, generate_recovery_token, confirm_recovery_token, \
+    resend_confirmation_token, confirm_resend_confirmation_token
 from .email import send_email
 from sqlalchemy.exc import IntegrityError
 from functools import wraps
@@ -15,7 +16,7 @@ def after_registration(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         if current_user.account_confirmed is True:
-            return redirect(url_for('index'))
+            return jsonify({'url': '/'})
         return f(*args, **kwargs)
     return wrap
 
@@ -26,117 +27,119 @@ def complete_registration(f):
     def wrap(*args, **kwargs):
 
         if current_user.account_confirmed is False:
-            return redirect(url_for('complete_signup'))
+            return jsonify({'url': '/'})
         else:
             return f(*args, **kwargs)
 
     return wrap
 
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return make_response(jsonify({'error': 'Page not found',
-                                'code': 404)}
+    return make_response(jsonify({'error': 'Page not found'}), 404)
     # return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def server_error(e):
-    return make_response(jsonify({'error':'Internal server error',
-                                'code':500})
+    return make_response(jsonify(error='Internal server error'), 500)
     # return render_template('500'.html'), 500
+
+
+@app.errorhandler(400)
+def link_error(e):
+    return make_response(jsonify(error='This link is broken or has expired'))
+
 
 @app.before_request
 def before_request():
     g.user = current_user
-        
-                         
+
+
 @app.route('/')
 @login_required
 @complete_registration
 def index():
-    return jsonify({
-        'code': 200,
-        'status': 'okay'
-    })
-
+    return jsonify({'code': 200, 'status': 'okay'})
 
     #  return render_template('index.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(full_name=form.full_name.data, email=form.email.data, username=form.username.data, password=form.password.data,
-                    email_confirmed=False)
-        try:
-            db.session.add(user)
-            db.session.commit()
-        except IntegrityError:
-            db.session.rollback()
 
-            #  The following lines of code
-            #  is the confirmation email being sent
-            #  to the  user
+    if current_user.is_authenticated:
+        return jsonify({'url': '/', 'msg': 'you are already login, you cant perform this action'})
 
-        token = generate_confirmation_token(user.email)
+    form = RegistrationForm(request.form)
 
-        confirm_url = url_for('confirm_email', token=token, _external=True)
+    if request.method == 'POST':
 
-        html = render_template('activate.html', confirm_url=confirm_url)
+        if form.validate_on_submit():
+            user = User(full_name=request.form['full_name'], email=request.form['email'], username=request.form['username'], password=request.form['password'],
+                        email_confirmed=False, account_confirmed=False)
+            try:
+                db.session.add(user)
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
 
-        subject = 'Please confirm your Email'
+                #  The following lines of code
+                #  is the confirmation email being sent
+                #  to the  user
 
-        # send_email function was imported from
-        # the email file
+            token = generate_confirmation_token(user.email)
 
-        send_email(user.email, subject, html)
+            confirm_url = url_for('confirm_email', token=token, _external=True)
+            html = render_template('activate.html', confirm_url=confirm_url)
 
-        return jsonify({
-            'status': True,
-            'code': 200,
-            'msg': 'A confirmation email has been sent to you',
-            'url':  '/complete/signup'
-        })
+            subject = 'Please confirm your Email'
 
-    else:
+            # send_email function was imported from
+            #  the email file
+            #
+            send_email(user.email, subject, html)
 
-        return jsonify({
-            'status': False,
-            'msg': 'Please check your post form'
-        })
+            flash('A mail has been sent to you')
 
-    #  return render_template('register.html', form=form)
+            return jsonify({'code': 200,
+                            'url': '/login'})
+
+        else:
+
+            return jsonify({'msg': 'something wrong occurred',
+                            'url': '/signup'})
+
+    # return render_template('register.html', form=form)
+
 
 @app.route('/complete/signup', methods=['GET', 'POST'])
 @login_required
 @after_registration
 def complete_signup():
 
-    form = CompleteForm()
+    form = CompleteForm(request.form)
 
-    if form.validate_on_submit():
-        g.user = PrivateDetails(address=form.address.data, city=form.city.data, state=form.state.data,
-                                postal_code=form.postal_code.data, phone_number=form.phone_number.data,
-                                date_of_birth=form.date_of_birth.data)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            g.user = PrivateDetails(address=request.form['address'], city=request.form['city'],
+                                    state=request.form['state'],
+                                    postal_code=request.form['postal_code'], phone_number=request.form['phone_number'],
+                                    date_of_birth=request.form['date_of_birth'])
 
-        current_user.account_confirmed = True
+            current_user.account_confirmed = True
+            db.session.add(g.user)
+            db.session.commit()
 
-        db.session.add(g.user)
-        db.session.commit()
+            return jsonify({'code': 200,
+                            'url': '/',
+                            'status': 'okay'})
+        else:
+            return jsonify({'url': '/complete/signup',
+                            'status': 'Error ',
+                            'msg': 'check post data'})
 
-        return jsonify({
-            'status': True,
-            'msg': 'success',
-            'url': '/index'
-        })
-
-    else:
-        return jsonify({
-            'status': False,
-            'msg': 'check your post form'
-        })
-
+    # return render_template('test.html', form=form)
 
 
 @app.route('/confirm/<token>')
@@ -150,12 +153,11 @@ def confirm_email(token):
     """
     try:
         email = confirm_token(token)
-    except Exception as e:
+    except:
         return jsonify({
             'msg': 'The confirmation link is invalid or has expired, danger',
             'status': False,
-            'code': 404
-        })
+            'code': 404})
         # flash('The confirmation link is invalid or has expired.', 'danger')
         #  abort(404)
 
@@ -177,82 +179,138 @@ def confirm_email(token):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-                         
-                         
-   if current_user.is_authenticated and current_user.email_confirmed:
+
+    if current_user.is_authenticated and current_user.email_confirmed:
         return jsonify({
             'msg': 'user already logged in',
-            'url': '/index'
-        })
-                             
-    form = LoginForm()
+            'url': '/index'})
 
-    if form.validate_on_submit():
+    form = LoginForm(request.form)
 
-        user = User.query.filter_by(username=form.username.data).first()
+    if request.method == 'POST':
 
-        if user is not None and user.verify_password(form.password.data):
+        if form.validate_on_submit():
 
-            if user.email_confirmed:
+            user = User.query.filter_by(username=request.form['username']).first()
 
-                login_user(user, form.remember_me.data)
+            if user is not None and user.verify_password(request.form['password']):
 
-                return jsonify({
-                    'status': True,
-                    'code': 200,
-                    'msg': 'Login Successful',
-                    'url': url_for(request.args.get('next') or 'index')
-                })
-                #  return redirect(request.args.get('next') or url_for('index'))
-            return jsonify({
-                'status': False,
-                'msg': 'Email has/nt been verified, you can/t login at the moment',
-                'url': '/'
-            })
-            #  flash('Mail not configured')
+                if user.email_confirmed:
+
+                    login_user(user, remember=True)
+
+                    return redirect(request.args.get('next') or url_for('index'))
+
+                else:
+                    flash('Your email hasnt been verified yet please verify your email to login')
+
+            else:
+
+                flash('invalid login credentials')
         else:
-            return jsonify({
-                'msg': 'Invalid Login Credentials',
-                'status': False,
-                'url': '/login'
-            }
-            )
-    else:
-        return jsonify({
-            'status': False,
-            'msg': 'check your post form'
-        })
-        #  flash('invalid login credentials')
+            return jsonify({'status': 'Error',
+                            'msg': 'check post form',
+                            'url': '/login'})
+
     # return render_template('login.html', form=form)
+
+
+@app.route('/resend/email', methods=['GET', 'POST'])
+def resend_email():
+
+    if current_user.is_authenticated:
+        return jsonify({'url': '/',
+                        'msg': 'user already logged in'})
+
+    form = ResendEmailForm(request.form)
+
+    if request.method == 'POST':
+
+        if form.validate_on_submit():
+
+            user = User.query.filter_by(email=request.form['email']).first()
+
+            token = resend_confirmation_token(user.email)
+
+            subject = 'Please confirm your email'
+
+            confirm_url = url_for('confirm_recovery_email', token=token, _external=True)
+
+            html = render_template('resend_email_confirms.html', confirm_url=confirm_url)
+
+            send_email(user.email, subject, html)
+
+            flash('A confirmation email has been sent to you')
+
+            return jsonify({'url': '/login'})
+        else:
+            return  jsonify({'status': 'error',
+                             'msg': 'check post form',
+                             'url': '/resend/email'})
+
+    # return render_template('resend_email.html', form=form)
+
+
+@app.route('/confirm/resend-email/<token>')
+def confirm_recovery_email(token):
+    try:
+        email = confirm_resend_confirmation_token(token)
+    except Exception as e:
+        return jsonify({
+            'msg': 'The confirmation link is invalid or has expired, danger',
+            'status': False,
+            'code': 400
+        })
+
+    user = User.query.filter_by(email=email).first()
+    if user.email_confirmed:
+        return jsonify(message='Email has already benn confirmed please login')
+    else:
+        user.email_confirmed = True
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({
+            'status': True,
+            'msg': 'Your email has been confirmed',
+            'url': '/login'
+        })
+    #  return r
 
 
 @app.route('/reset/password', methods=['GET', 'POST'])
 def reset():
-    form = EmailForm()
 
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
 
-        if user and user.email_confirmed:
+    form = EmailForm(request.form)
 
-            subject = 'Password reset requested'
+    if request.method == 'POST':
 
-            token = generate_recovery_token(user.email)
+        if form.validate_on_submit():
 
-            recover_url = url_for('reset_password_with_token', token=token, _external=True)
+            user = User.query.filter_by(email=request.form['email']).first()
 
-            html = render_template('recover.html', recover_url=recover_url)
+            if user and user.email_confirmed:
 
-            send_email(user.email, subject, html)
+                subject = 'Password reset requested'
 
-            flash('A confirmation link to reset your password has been sent to you')
+                token = generate_recovery_token(user.email)
 
-            return redirect(url_for('login'))
+                recover_url = url_for('reset_password_with_token', token=token, _external=True)
 
-        else:
-            flash('This Email hasnt been confirmed yet')
+                html = render_template('recover.html', recover_url=recover_url)
 
-    return render_template('reset.html', form=form)
+                send_email(user.email, subject, html)
+
+                flash('A confirmation link to reset your password has been sent to you')
+
+                return jsonify({'url': '/login'})
+
+            else:
+                return jsonify({'msg': 'This email has\nt been confirmed yet'})
+
+    #  return render_template('reset.html', form=form)
 
 
 @app.route('/reset/<token>', methods=['GET', 'POST'])
@@ -261,19 +319,23 @@ def reset_password_with_token(token):
         email = confirm_recovery_token(token)
 
     except:
-        flash('The confirmation link is invalid or has expired.', 'danger')
-        abort(400)
+        return jsonify({
+            'msg': 'The confirmation link is invalid or has expired, danger',
+            'status': False,
+            'code': 400})
 
-    form = PasswordForm()
+    form = PasswordForm(request.form)
 
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=email).first()
-        user.password = form.password.data
+    if request.method == 'POST':
 
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('reset_password.html', form=form, token=token)
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=request.form['email']).first()
+            user.password = request.form['password']
+
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({'url': '/login'})
+    # return render_template('reset_password.html', form=form, token=token)
 
 
 @app.route('/logout')
@@ -282,6 +344,6 @@ def logout():
     logout_user()
     return jsonify({
         'msg': 'You have been logged out',
-        'url': '/login'
+        'url': url_for('login')
     })
     # return redirect(url_for('login'))
